7c731a26db345b3d65a6fed56f7fa7c05665e1a6
Saad Iqbal <saad.j.iqbal@gmail.com>
2024-09-10 01:01:54 +1200

    Added some functionality for dynamic instantiation

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
+var dog_scene = preload("res://scenes/dog.tscn")
+
 func _ready() -> void:
-	pass
+	var dog_scene_instance = dog_scene.instantiate()
+	add_child(dog_scene_instance)
