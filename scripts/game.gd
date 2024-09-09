37d336367aca176abd31cab54079db30ddea4aaa
Saad Iqbal <saad.j.iqbal@gmail.com>
2024-09-10 01:14:51 +1200

    Added some comments

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+# Functionality will be something like using RNG to spawn
+# animals.
+#
+# Multiple animals & different animals as the levels progress.
 var dog_scene = preload("res://scenes/dog.tscn")
 
 func _ready() -> void:
