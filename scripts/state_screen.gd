1f7b498895ed908cc41a6be55609ca8547e92fef
lusternite <luster_nite@hotmail.com>
2024-09-10 22:16:14 +0900

    Finalised testing for screenstates
    Implemented visibility toggling functions for the start and game over screens.
    Tested with the quit button, ready for linking into proper start game/game over functionality when ready.

--- scripts/state_screen.gd
+++ scripts/state_screen.gd
@@ -1,11 +1,47 @@
 extends Control
 
+var startButtonRef
+var startGameLabelRef
+var quitGameButtonRef
+var restartButtonRef
+var gameOverLabelRef
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	startButtonRef = find_child("Start button")
+	quitGameButtonRef = find_child("Quit button")
+	startGameLabelRef = find_child("Start Game label")
+	restartButtonRef = find_child("Restart button")
+	gameOverLabelRef = find_child("Game Over label")
+	
+	enableGameOverScreen()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+# Turns the UI elements related to the start screen visible.
+func enableStartScreen() -> void:
+	startButtonRef.visible = true
+	startGameLabelRef.visible = true
+	quitGameButtonRef.visible = true
+
+func enableGameOverScreen() -> void:
+	gameOverLabelRef.visible = true
+	restartButtonRef.visible = true
+	quitGameButtonRef.visible = true
+
+func hideStateScreen() -> void:
+	startButtonRef.visible = false
+	startGameLabelRef.visible = false
+	quitGameButtonRef.visible = false
+	gameOverLabelRef.visible = false
+	restartButtonRef.visible = false
+
+
+func _on_quit_button_pressed() -> void:
+	hideStateScreen()
