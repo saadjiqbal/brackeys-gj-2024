78063427f1b1bf7c6a85226d0ed644a8af3473cd
Saad Iqbal <saad.j.iqbal@gmail.com>
2024-09-15 20:08:05 +1200

    New background and updated styling

--- scripts/game_win.gd
+++ scripts/game_win.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.visible = false
+	pass# self.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
