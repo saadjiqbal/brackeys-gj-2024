627700a4f652f4081cf34a77b37bac9df63083a4
Saad Iqbal <saad.j.iqbal@gmail.com>
2024-09-15 20:12:50 +1200

    Changed visibility

--- scripts/game_win.gd
+++ scripts/game_win.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass# self.visible = false
+	self.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
